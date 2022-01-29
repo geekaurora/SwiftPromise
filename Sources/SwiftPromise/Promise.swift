cc2b95a263536ab8a5d2a10022bf896c41e14301
Cheng Zhang <showt2me@gmail.com>
2022-01-29 18:13:54 +0900

    minor

--- Sources/SwiftPromise/Promise.swift
+++ Sources/SwiftPromise/Promise.swift
@@ -1,45 +1,5 @@
 import Foundation
 
-/**
- Promise library that manages asynchronous tasks elegantly.
- 
- ### Usage
- 
- 1. then()/catch():
- ```
- let promise = Promise<String> { (resolve, reject) in
- self.delayAsync {
- resolve("result")
- }
- }
- promise
- .then { result in
- print(result)
- }
- .catch { error in
- print(error)
- }
- ```
- If `then()` returns a new Promise, it will be executed correspondingly.
- 
- 2. await():
- ```
- let result = promise.await()
- print(result)
- ```
- 
- 3. all():
- ```
- let promises = [createPromise(), createPromise(), createPromise()]
- 
- Promise.all(promises)
- .then { _ in
- print("Completed all promises successfully.")
- }.catch { error in
- print("Failed to execute promises. Error - \(error)")
- }
- ```
- */
 public class Promise {
   public typealias Input = Any
   public typealias Result = Any
@@ -98,8 +58,7 @@ public class Promise {
     // Start pre-preExecution `preExecution`: the real preExecution will be when resolve() / reject() gets called.
     preExecution(resolve, reject)
     
-    // 3. Return new Promise.
-    return nextPromise!
+    return self
   }
   
   /// `then` function that will be called on `resolve()`.
