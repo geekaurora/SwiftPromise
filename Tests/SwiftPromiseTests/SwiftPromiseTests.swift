0ee9d3304f12c3c504f16920b4e735731b74b9b2
Cheng Zhang <756876+geekaurora@users.noreply.github.com>
2022-01-29 23:36:15 +0900

    [Major] Improve the support of chaining async then. (#4)
    * clean up.
    
    * [Major] Remove generic Type from Promise.
    
    * minor
    
    * thenClosures
    
    * built
    
    * Code: finished
    
    * testSingleThenResolve: passed
    
    * dequeNextThenClosures()
    
    * rootPromise
    
    * testChainingThenResolve(): passed
    
    * testSingleThenResolveAsynchronously: passed
    
    * testChainingThenResolve(): failed
    
    * minor
    
    * minor
    
    * hasQueuedThenClosure
    
    * preExecution(): calls rootPromis, not current
    
    * [Major] testChainingThenResolveSync - passed!
    
    * [MileStone] testChainingThenResolveAsynchronously(): passed
    
    * clean up
    
    * [MileStone] all tests: passed!

--- Tests/SwiftPromiseTests/SwiftPromiseTests.swift
+++ Tests/SwiftPromiseTests/SwiftPromiseTests.swift
@@ -1,4 +1,5 @@
 import XCTest
+import CZUtils
 import CZTestUtils
 @testable import SwiftPromise
 
@@ -7,20 +8,41 @@ final class SwiftPromiseTests: XCTestCase {
   static let firstThenPromiseResult = "firstThenPromiseResult"
   static let secondThenPromiseResult = "secondThenPromiseResult"
   static let error: Error? = NSError(domain: "Error", code: 999, userInfo: nil)
-  static let asyncDelay: TimeInterval = 0.1
+  static let asyncDelay: TimeInterval = 0.01
   static let fulfillWaitInterval: TimeInterval = 30
   
   // MARK: - Test resolve() / reject()
   
-  func testResolve() {
+  func testSingleThenResolve() {
     let (waitExpectation, expectation) = CZTestUtils.waitWithInterval(Self.fulfillWaitInterval, testCase: self)
     // Init promise.
     let promise = createPromise(shouldAsync: false)
     
     promise
-      .then { (result) -> Promise<String> in
-        return Promise<String> { (resolve, reject) in
-          XCTAssertTrue(result == Self.result, "Actual result = \(result); Expected result = \(Self.result)")
+      .then { (result) -> Promise in
+        return Promise(root: promise) { (resolve, reject) in
+          XCTAssertTrue(result as! String == Self.result, "Actual result = \(result); Expected result = \(Self.result)")
+          
+          // Call resolve() with the result for the next Promise
+          resolve(Self.firstThenPromiseResult)
+          expectation.fulfill()
+        }
+        
+      }
+    
+    // Wait for asynchronous result.
+    waitExpectation()
+  }
+  
+  func testSingleThenResolveAsynchronously() {
+    let (waitExpectation, expectation) = CZTestUtils.waitWithInterval(Self.fulfillWaitInterval, testCase: self)
+    // Init promise.
+    let promise = createPromise(shouldAsync: true)
+    
+    promise
+      .then { (result) -> Promise in
+        return Promise(root: promise) { (resolve, reject) in
+          XCTAssertTrue(result as! String == Self.result, "Actual result = \(result); Expected result = \(Self.result)")
           
           // Call resolve() with the result for the next Promise
           resolve(Self.firstThenPromiseResult)
@@ -36,150 +58,70 @@ final class SwiftPromiseTests: XCTestCase {
   /**
    Test chaining multitple `then` - returns Promise.
    */
-//  func testChainingThenResolve() {
-//    let (waitExpectation, expectation) = CZTestUtils.waitWithInterval(Self.fulfillWaitInterval, testCase: self)
-//    // Init promise.
-//    let promise = createPromise(shouldAsync: false)
-//
-//    // Test chaining then().
-//    promise
-//      .then { (result) -> Promise<String> in
-//        return Promise<String> { (resolve, reject) in
-//          XCTAssertTrue(result == Self.result, "Actual result = \(result); Expected result = \(Self.result)")
-//
-//          // Call resolve() with the result for the next Promise
-//          resolve(Self.firstThenPromiseResult)
-//          expectation.fulfill()
-//        }
-//
-//      }
-//    // * Note: next then() will call the same promise, as prev then() method returns `self`.
-////      .then { (result) -> String? in
-////        XCTAssertEqual(result as! String, Self.chainingThenPromiseResult)
-////        expectation.fulfill()
-////        return nil
-////      }
-//
-//    // Wait for asynchronous result.
-//    waitExpectation()
-//  }
+  func testChainingThenResolve() {
+    let (waitExpectation, expectation) = CZTestUtils.waitWithInterval(Self.fulfillWaitInterval, testCase: self)
+    
+    let promise = createPromise(shouldAsync: false)
+    promise
+      .then { (result) -> Promise in
+        return Promise(root: promise) { (resolve, reject) in
+          dbgPrint("[Debug] Finished the first then().")
+          XCTAssertEqual(result as! String, Self.result)
+          
+          resolve(Self.firstThenPromiseResult)
+        }
+      }
+      .then { (result) -> Promise in
+        return Promise(root: promise) { (resolve, reject) in
+          dbgPrint("[Debug] Finished the first then().")
+          XCTAssertEqual(result as! String, Self.firstThenPromiseResult)
+          
+          resolve(Self.secondThenPromiseResult)
+          expectation.fulfill()
+        }
+      }
+    
+    waitExpectation()
+  }
   
+  func testChainingThenResolveAsynchronously() {
+    let (waitExpectation, expectation) = CZTestUtils.waitWithInterval(Self.fulfillWaitInterval, testCase: self)
+    
+    let promise = createPromise(shouldAsync: true)
+    promise
+      .then { (result) -> Promise in
+        return Promise(root: promise) { (resolve, reject) in
+          self.delayAsync {
+            dbgPrint("[Debug] Finished the first then().")
+            XCTAssertEqual(result as! String, Self.result)
+            
+            resolve(Self.firstThenPromiseResult)
+          }
+        }
+      }
+      .then { (result) -> Promise in
+        return Promise(root: promise) { (resolve, reject) in
+          self.delayAsync {
+            dbgPrint("[Debug] Finished the first then().")
+            XCTAssertEqual(result as! String, Self.firstThenPromiseResult)
+            
+            resolve(Self.secondThenPromiseResult)
+            expectation.fulfill()
+          }
+        }
+      }
+    
+    waitExpectation()
+  }
   
-//  /**
-//   Test chaining multitple `then` - returns value.
-//   */
-//  func testChainingThenResolve() {
-//    let (waitExpectation, expectation) = CZTestUtils.waitWithInterval(Self.fulfillWaitInterval, testCase: self)
-//    // Init promise.
-//    let promise = createPromise(shouldAsync: false)
-//
-//    // Test chaining then().
-//    promise
-//      .then { (result) -> String? in
-//        print("First then")
-//        XCTAssertTrue(result == Self.result, "Actual result = \(result); Expected result = \(Self.result)")
-//        return Self.chainingThenPromiseResult
-//      }
-//    // * Note: next then() will call the same promise, as prev then() method returns `self`.
-//      .then { (result) -> String? in
-//        XCTAssertEqual(result as! String, Self.chainingThenPromiseResult)
-//        expectation.fulfill()
-//        return nil
-//      }
-//
-//    // Wait for asynchronous result.
-//    waitExpectation()
-//  }
-
-//  func testReject() {
-//    let (waitExpectation, expectation) = CZTestUtils.waitWithInterval(Self.fulfillWaitInterval, testCase: self)
-//    // Init promise.
-//    let promise = createPromise(shouldReject: true)
-//
-//    // Test catch().
-//    promise.then { result in
-//      XCTAssert(false, "then() shouldn't be called.")
-//      return nil
-//    }
-//    .catch { error in
-//      XCTAssertTrue(error as AnyObject === Self.error as AnyObject , "Actual result = \(error); Expected result = \(Self.error)")
-//      expectation.fulfill()
-//    }
-//    // Wait for asynchronous result.
-//    waitExpectation()
-//  }
-//
-
-//
-//  // MARK: - Test all()
-//
-//  func testAllPromisesResolve() {
-//    let (waitExpectation, expectation) = CZTestUtils.waitWithInterval(Self.fulfillWaitInterval, testCase: self)
-//    // Init promise.
-//    let promises = [createPromise(), createPromise()]
-//
-//    // Test then().
-//    Promise.all(promises).then { (result) in
-//      XCTAssertTrue(
-//        result == Promise<Any>.allPromisesSuccessString,
-//        "Actual result = \(result); Expected result = \(Promise<Any>.allPromisesSuccessString)")
-//      expectation.fulfill()
-//      return nil
-//    }
-//    // Wait for asynchronous result.
-//    waitExpectation()
-//  }
-//
-//  func testAllPromisesReject() {
-//    let (waitExpectation, expectation) = CZTestUtils.waitWithInterval(Self.fulfillWaitInterval, testCase: self)
-//    // Init promise.
-//    let promises = [createPromise(), createPromise(), createPromise(shouldReject: true)]
-//
-//    // Test then().
-//    Promise.all(promises).then { _ in
-//      XCTAssertTrue(false, "then() shouldn't be called.")
-//      return nil
-//    }.catch { error in
-//      XCTAssertTrue(error as AnyObject === Self.error as AnyObject , "Actual result = \(error); Expected result = \(Self.error)")
-//      expectation.fulfill()
-//    }
-//    // Wait for asynchronous result.
-//    waitExpectation()
-//  }
-//
-//  // MARK: - Test await()
-//
-//  func testAwaitWithResolve() {
-//    let (waitExpectation, expectation) = CZTestUtils.waitWithInterval(Self.fulfillWaitInterval, testCase: self)
-//    // Init promise.
-//    let promise = createPromise()
-//    // Test await().
-//    let result = promise.await()
-//    XCTAssertTrue(result == Self.result, "Actual result = \(result); Expected result = \(Self.result)")
-//    expectation.fulfill()
-//    // Wait for asynchronous result.
-//    waitExpectation()
-//  }
-//
-//  func testAwaitWithReject() {
-//    let (waitExpectation, expectation) = CZTestUtils.waitWithInterval(Self.fulfillWaitInterval, testCase: self)
-//    // Init promise.
-//    let promise = createPromise(shouldReject: true)
-//    // Test await().
-//    let result = promise.await()
-//    XCTAssertTrue(result == nil, "Actual result = \(result); Expected result = nil")
-//    expectation.fulfill()
-//    // Wait for asynchronous result.
-//    waitExpectation()
-//  }
 }
 
 // MARK: - Convenience methods
 
 private extension SwiftPromiseTests {
-
-  func createPromise(shouldAsync: Bool = true, shouldReject: Bool = false) -> Promise<String> {
-    let promise = Promise<String> { (resolve, reject) in
+  
+  func createPromise(shouldAsync: Bool = true, shouldReject: Bool = false) -> Promise {
+    let promise = Promise(root: nil) { (resolve, reject) in
       self.delayAsync(shouldAsync: shouldAsync) {
         if shouldReject {
           reject(Self.error)
@@ -193,7 +135,8 @@ private extension SwiftPromiseTests {
   
   func delayAsync(shouldAsync: Bool = true, _ closure: @escaping () -> Void) {
     if shouldAsync {
-      DispatchQueue.global().asyncAfter(deadline: .now() + Self.asyncDelay, execute: closure)
+      // DispatchQueue.global().asyncAfter(deadline: .now() + Self.asyncDelay, execute: closure)
+      DispatchQueue.main.asyncAfter(deadline: .now() + Self.asyncDelay, execute: closure)
     } else {
       closure()
     }
